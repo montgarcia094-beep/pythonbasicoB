ef13347497363f79709cbd59752d265a431daf85
montgarcia094-beep <montgarcia094@gmail.com>
2026-02-26 18:45:35 -0800

    Guardando mi sesion zombie

--- OOP/Zombie.py
+++ OOP/Zombie.py
@@ -1,22 +1,12 @@
-class Enemigo:
-    tipo_enemigo: str
-    puntos_energia: int = 10
-    ataque = 1
-    
-    def __init__(self, tipo_enemigo, puntos_energia=10, ataque=1):
-        self.tipo_enemigo  = tipo_enemigo
-        self.puntos_energia = puntos_energia
-        self.ataque = ataque
+from Enemigo import *
 
-    def get_tipo_enemigo(self):
-        return self.tipo_enemigo
-    
-    def habla(self):
-        print(f"yo son {self.tipo_enemigo}. Preparando para pelear!!")
-
-    def camina(self):
-        print(f"{self.tipo_enemigo} se mueve cerca de ti!!")
-
-    def atacar(self):
-        print(f"{self.tipo_enemigo} ataca con un {self.ataque} de daño!!")
+class Zombie(Enemigo):
+    def __init__(self, puntos_enemigo=10, ataque=1):
+        super().__init__(tipo_enemigo='Zombie', punto_energia=puntos_enemigo, ataque=ataque)
+        
+        def habla(self):
+            print("*Hummmm.....*")
+        def propagar_enfermedad(self):
+            print("El zombie esta tratando de propagar la enfermedad!!")
+            
 
